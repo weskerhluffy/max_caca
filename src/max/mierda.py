4b81230db9fc3b994a757ca2774ecb9c75f1e1c1
ernesto.alvarado@oracle.com <ealvarad@slc02oyd.us.oracle.com>
2016-11-28 16:02:55 -0800

    psaa con quickshit

--- src/max/mierda.py
+++ src/max/mierda.py
@@ -6,51 +6,36 @@ Created on 27/11/2016
 import sys
 import logging
 nivel_log = logging.ERROR
-# nivel_log = logging.DEBUG
+#nivel_log = logging.DEBUG
+logger_cagada = None
 
-def crea_monton(numeros):
-    for idx_num, nume in enumerate(numeros):
-        num_ultimo = nume
-        idx_act = idx_num
-        while idx_act:
-            num_act = numeros[(idx_act - 1) >> 1]
-            if num_ultimo > num_act:
-                numeros[idx_act] = num_act
-            else:
-                break
-            idx_act = (idx_act - 1) >> 1
-        assert numeros[idx_act] <= num_ultimo
-        numeros[idx_act] = num_ultimo
+def ordena_rapido_particionar(numeros,izq,der,pivote):
+	idx_ordenados=izq-1
+	for idx_actual in range(izq,der):
+		if(numeros[idx_actual]<=pivote):
+			idx_ordenados+=1
+			temp=numeros[idx_ordenados]
+			numeros[idx_ordenados]=numeros[idx_actual]
+			numeros[idx_actual]=temp
 
+	numeros[der]=numeros[idx_ordenados+1]
+	idx_ordenados+=1
+	numeros[idx_ordenados]=pivote
+	return idx_ordenados
+	
 
-def monton_sort(numeros):
-    tam_nume = len(numeros)
-    crea_monton(numeros)
-
-
-    for idx_num, nume in enumerate(numeros):
-        limite_heap = tam_nume - idx_num - 1
-        num_ultimo = numeros[limite_heap]
-        idx_act = 0
-        numeros[limite_heap] = numeros[0]
-        assert not limite_heap or numeros[limite_heap] >= numeros[limite_heap - 1]
-        while ((idx_act << 1) | 1) < limite_heap:
-            idx_i = (idx_act << 1) | 1
-            idx_d = idx_i + 1
-            if(idx_i < limite_heap):
-                idx_maior = idx_i
-            if(idx_d < limite_heap) and numeros[idx_i] < numeros[idx_d]:
-                    idx_maior = idx_d
-            num_act = numeros[idx_maior]
-                
-            if num_ultimo < num_act:
-                numeros[idx_act] = num_act
-            else:
-                break
-            idx_act = idx_maior
-        numeros[idx_act] = num_ultimo
-
-    return numeros
+def ordena_rapido(numeros,izq,der):
+	logger_cagada.debug("la izq %u der %u"%(izq,der))
+	
+	if(der-izq<=0):
+		return
+	else:
+		pivote=numeros[der]
+		pivote_nuevo=ordena_rapido_particionar(numeros,izq,der,pivote)
+		logger_cagada.debug("el pivote nuevo %u"%pivote_nuevo)
+		ordena_rapido(numeros,izq,pivote_nuevo-1)
+		ordena_rapido(numeros,pivote_nuevo+1,der)
+	return numeros
 
 
 def max_mieda_core(numeros, tam_ventana):
@@ -59,13 +44,16 @@ def max_mieda_core(numeros, tam_ventana):
     salto_idx = tam_ventana - 1
     min_caca = 0
     
-    numeros_ord = monton_sort(numeros)
+#    numeros_ord = monton_sort(numeros)
 #    numeros_ord = sorted(numeros)
+    numeros_ord = ordena_rapido(numeros,0,len(numeros)-1)
+    logger_cagada.debug("go %s" % numeros_ord)
     
     min_caca = numeros_ord[tam_ventana - 1] - numeros_ord[0]
     for idx_nume, numero in enumerate(numeros_ord[:idx_limite + 1]):
         min_actual = 0
         idx_final = idx_nume + salto_idx
+        logger_cagada.debug("when there is %u %u" % (idx_nume, idx_final))
         min_actual = numeros_ord[idx_final] - numero
         if(min_actual < min_caca):
             min_caca = min_actual
@@ -81,6 +69,7 @@ def max_mierda_main():
     for caca in lineas[2:]:
         numeros.append(int(caca))
     
+    logger_cagada.debug("teen titans %s" % numeros)
     
     res = max_mieda_core(numeros, tam_ventana)
     print(res)
@@ -90,5 +79,6 @@ def max_mierda_main():
 if __name__ == '__main__':
         FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
         logging.basicConfig(level=nivel_log, format=FORMAT)
+        logger_cagada = logging.getLogger("asa")
+        logger_cagada.setLevel(nivel_log)
         max_mierda_main()
-
